baa3f71cbca7dbab37c529ed013b44eab710bbf8
Vikash Kumar <vikaskumarkxz@gmail.com>
2026-09-22 03:09:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-}
+	let s1Lower = s1.toLowerCase()
+	let s2Lower = s2.toLowerCase()
 
-// Please do not change the code below
+	return s1Lower.indexOf(s2Lower)
+
+	// let res = -1
+	// let char2 = s2Lower.charAt(0)
+
+	// for (let index = 0; index < s1.length; index++) {
+	// 	let char1 = s1Lower.charAt(index) 
+	// 	if(char1 === char2){
+	// 		res = index 
+	// 	} 
+	// 	return res;
+	// } 
+	
+}
+// console.log(indexOfIgnoreCase(s1, s2))
+
+// Please do not change the code below 
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
